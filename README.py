7302a91de9f2ecfc2a20d0489c950e1bb62df27b
Noah1599 <115689277+Noah1599@users.noreply.github.com>
2025-03-02 15:54:43 +0700

    set public

--- README.py
+++ README.py
@@ -1,3 +1,7 @@
 #"This is the the readme file for w3resorce.com python exercises"
 #"website url https://www.w3resource.com/python-exercises/"
 #here i wil advance my python skills my creating more and more python goal is to be great at py
+
+#so far i have 
+#Total lines in folder: 474
+#Total characters in folder: 14400
