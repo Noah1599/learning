23848fe2dd4ab21cb4f31d9aa7982b1034cea4f0
Noah1599 <115689277+Noah1599@users.noreply.github.com>
2025-03-02 16:16:17 +0700

    done until 45
    now it becomes more hard

--- w3resorce/44 Python Site Packages Locator.py
+++ w3resorce/44 Python Site Packages Locator.py
@@ -1,1 +1,7 @@
 #Write a Python program to locate Python site packages.
+import site
+
+sitePakages=site.getsitepackages()
+print("this is where site packages are located")
+for directory in sitePakages:
+    print(directory)
