b0cd7d0a605bc16a2eadfd9b93ecc0cc23394c48
Noah1599 <115689277+Noah1599@users.noreply.github.com>
2025-03-02 15:50:57 +0700

    readme

---
+++ README.py
@@ -0,0 +1,3 @@
+#"This is the the readme file for w3resorce.com python exercises"
+#"website url https://www.w3resource.com/python-exercises/"
+#here i wil advance my python skills my creating more and more python goal is to be great at py
--- w3resorce/readme.py
+++
@@ -1,2 +0,0 @@
-print("This is the the readme file for w3resorce.com python exercises")
-print("website url https://www.w3resource.com/python-exercises/")
