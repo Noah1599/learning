ce8373735711a8d063c3228f5b82feb490e7cc2d
Noah1599 <115689277+Noah1599@users.noreply.github.com>
2025-03-03 20:52:10 +0700

    1 done
    to late

---
+++ w3resorce/51 Program Profiler.py
@@ -0,0 +1,11 @@
+#Write a Python program to determine the profiling of Python programs.
+#Note: A profile is a set of statistics that describes how often and 
+#for how long various parts of the program executed. These statistics 
+# can be formatted into reports via the pstats module
+
+import cProfile
+
+def cal():
+    print(2+3+4)
+
+cProfile.run('cal')
