8ace48f190f893534b49aee04d323b4bdf6e44a2
Noah1599 <115689277+Noah1599@users.noreply.github.com>
2025-03-09 18:56:28 +0700

    finnish for day

--- Leetcode/1 two sums.py
+++ Leetcode/1 two sums.py
@@ -1,10 +1,27 @@
-class Solution(object):
-    def twoSum(self,nums,target):
-        n = len(nums)
-        for i in range(n - 1):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []
 
-    print(twoSum(0,[2,7,11,15],9))    
+#this takes O(n^2)
+#class Solution(object):
+#    def twoSum(self,nums,target):
+#        n = len(nums)
+#        for i in range(n - 1):
+#            for j in range(i + 1, n):
+#                if nums[i] + nums[j] == target:
+#                    return [i, j]
+#        return []
+#
+#    print(twoSum(0,[2,7,11,15],9)) 
+# 
+# making faster now
+
+class Solution(object):
+    def twoSum(self,nums,target):   
+        hashmap={} #val : index
+
+        for i, n in enumerate(nums): #using enumerate i get index and value n
+            diff=target-n
+            if diff in hashmap:
+                return [hashmap[diff],i]
+            print(n)
+            hashmap[n]=i
+        return
+    print(twoSum(0,[2,7,11,15],9)) 
