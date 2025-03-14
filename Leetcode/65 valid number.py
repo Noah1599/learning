92d6d71ad952f2e4a3ac1cdcdb5bf6d163219f9f
Noah1599 <115689277+Noah1599@users.noreply.github.com>
2025-03-14 13:01:30 +0700

    first hard question

--- Leetcode/65 valid number.py
+++ Leetcode/65 valid number.py
@@ -1,8 +1,25 @@
 #https://leetcode.com/problems/valid-number/description/
+
 class Solution(object):
-    def isNumber(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        
+    def isNumber(self, s): 
+        try:
+            float(s)
+            return not s.lower() in {"inf", "+inf", "-inf", "infinity", "+infinity", "-infinity","nan","+nan","-nan"}
+        except ValueError: return False
+
+
+
+sol=Solution()
+
+print(sol.isNumber("2"))               # True
+print(sol.isNumber("-0.1"))            # True
+print(sol.isNumber("+3.14"))           # True
+print(sol.isNumber("-90E3"))           # True
+print(sol.isNumber("53.5e93"))         # True
+print(sol.isNumber("3e+7"))            # True
+print(sol.isNumber("-123.456e789"))    # True
+print(sol.isNumber("abc"))             # False
+print(sol.isNumber("+inf"))             # False
+print(sol.isNumber("inf"))             # False
+print(sol.isNumber("infinity"))             # False
+print(sol.isNumber("1a"))             # False
