8d190375385ed6b24a5053a4dd1c4fdb8641487a
Noah1599 <115689277+Noah1599@users.noreply.github.com>
2025-04-12 17:59:20 +0700

    Create 27 remove element.py

---
+++ Leetcode/27 remove element.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
+        count=0
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                nums[count]=nums[i]
+                count+=1
+        return count
+
+nums = [3,2,2,3]
+val = 3
+print(Solution().removeElement(nums,val))
