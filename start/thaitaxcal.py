a7169bb54ab7e03d399b626d0d9d597846eb8cf7
Noah1599 <115689277+Noah1599@users.noreply.github.com>
2025-04-19 18:56:05 +0700

    dd

--- start/thaitaxcal.py
+++ start/thaitaxcal.py
@@ -23,5 +23,17 @@ def calculate_thai_tax(income_thb):
 
     return (round(income_thb/5.08),round((income_thb-tax)/5.08),round(tax/5.08),round(100*(1-((income_thb-tax)/income_thb))))
 
-x1=800000*5.08
+x1=100000*5.08
+x2=200000*5.08
+x3=300000*5.08
+x4=400000*5.08
+x5=500000*5.08
+x6=600000*5.08
+x=134000*5.08
 print(calculate_thai_tax(x1))
+print(calculate_thai_tax(x2))
+print(calculate_thai_tax(x3))
+print(calculate_thai_tax(x4))
+print(calculate_thai_tax(x5))
+print(calculate_thai_tax(x6))
+print(calculate_thai_tax(x))
